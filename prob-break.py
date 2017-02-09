c8c2c11441c876b329ab127460fc7be67bcfcd30
Joshua Loo <joshuapeterloo@gmail.com>
2017-02-09 23:11:49 +0000

    Made prob-break.py work

--- prob-break.py
+++ prob-break.py
@@ -1,5 +1,4 @@
 from operator import add
-from collections import Counter,defaultdict
 from itertools import chain
 from random import shuffle
 
@@ -13,24 +12,20 @@ def process(tab,rounds,rm):
     return sorted(tab)
 
 def main():
-    rm = int(input("Number of rooms: "))
-    r = int(input("Number of rounds: "))
-    f = int(input("Number of breaking teams: "))
-    n = int(input("Number of iterations (recommended 1000+): "))
-    s = [process([[0]*rm*4][0],r,rm) for i in range(n)]
-    m = [br[-f:][0] for br in s]
-    e = [(m[i],s[i][-f:].count(m[i]),s[i].count(m[i])) for i in range(len(m))]
-    d = list(Counter(e).items())
-    o = defaultdict(list)
-    for y in d:
-        o[y[0][0]].append(y)
-    u = [[(y[0][0],y[0][1]*y[1],y[0][2]*y[1]) for y in x] for x in list(o.values())]
-    p = [[x[0][0]]+[sum(y) for y in list(zip(*x))[-2:]] for x in u]
-    c = [[x[0],round((x[1]/x[2])*100*x[2]/sum([y[2] for y in p]),1)] for x in p]
-    d = [round(100*x[2]/sum([y[2] for y in p]),1) for x in p]
-    i = [[c[x][0],sum([y for y in d[0:x]])+c[x][1]] for x in range(len(c))]
-    for b in i:
-        print("Teams on "+str(b[0])+" points have a "+str(b[1])+"% chance of breaking.")
+    rooms = int(input("Number of rooms: "))
+    rounds = int(input("Number of rounds: "))
+    final = int(input("Number of breaking teams: "))
+    iterate = int(input("Number of iterations (recommended 1000+): "))
+    sample = [process([[0]*rooms*4][0],rounds,rooms) for i in range(iterate)]
+    b = [x[-final:] for x in sample]
+    points = [[b[i][0],b[i].count(b[i][0]),sample[i].count(b[i][0])] for i in range(iterate)]
+    split = {}
+    for x in points:
+        split.setdefault(x[0], []).append(x)
+    nag = [[x[0][0],sum(x[1]),sum(x[2])] for x in [list(zip(*split[x])) for x in split.keys()]]
+    agd = [[nag[i][0],sum([y[2] for y in nag[:i]]),sum([y[2] for y in nag])] for i in range(len(nag))]
+    prob = [[nag[i][0],100*nag[i][1]/sum(x[2] for x in nag)+agd[i][1]/agd[i][2]] for i in range(len(nag))]
+    for point in prob:
+        print("Teams on",str(point[0]),"points have a",str(point[1])+"% chance of breaking.")
 
 main()
-
